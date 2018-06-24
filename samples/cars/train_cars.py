1ce4646903c731ed86f839cc5976fcbc551ea94c
root <root@vultr.guest>
2018-06-24 10:51:29 +0000

    modify args

--- samples/cars/train_cars.py
+++ samples/cars/train_cars.py
@@ -59,17 +59,18 @@ class CarsConfig(Config):
     IMAGE_MAX_DIM = 1069
 
     # Use smaller anchors because our image and objects are small
-    RPN_ANCHOR_SCALES = (8, 16, 32, 64, 128)  # anchor side in pixels
+    #RPN_ANCHOR_SCALES = (8, 16, 32, 64, 128)  # anchor side in pixels
 
     # Reduce training ROIs per image because the images are small and have
     # few objects. Aim to allow ROI sampling to pick 33% positive ROIs.
-    TRAIN_ROIS_PER_IMAGE = 32
+    
+    #TRAIN_ROIS_PER_IMAGE = 32
 
     # Use a small epoch since the data is simple
-    STEPS_PER_EPOCH = 400
+    STEPS_PER_EPOCH = 100
 
     # use small validation steps since the epoch is small
-    VALIDATION_STEPS = 20
+   # VALIDATION_STEPS = 20
 
 def get_ax(rows=1, cols=1, size=8):
     """Return a Matplotlib Axes array to be used in
