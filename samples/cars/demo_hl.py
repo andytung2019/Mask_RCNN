a64c82ff671d6c86618840b8920834f945d7828e
root <root@vultr.guest>
2018-06-25 17:37:24 +0000

    change demo_hl

--- samples/cars/demo_hl.py
+++ samples/cars/demo_hl.py
@@ -48,23 +48,13 @@ class MyCheck():
         model = modellib.MaskRCNN(mode="inference", model_dir=MODEL_DIR, config=config)
 
         # Load weights trained on MS-COCO
-        model.load_weights(COCO_MODEL_PATH, by_name=True)
+        model.load_weights(COCO_MODEL_PATH, by_name=True, exclude =[
+		"mrcnn_class_logits", "mrcnn_bbox_fc", "mrcnn_bbox","mrcnn_mask"
+	])
 
-        class_names = ['BG', 'person', 'bicycle', 'car', 'motorcycle', 'airplane',
-                       'bus', 'train', 'truck', 'boat', 'traffic light',
-                       'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird',
-                       'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear',
-                       'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie',
-                       'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball',
-                       'kite', 'baseball bat', 'baseball glove', 'skateboard',
-                       'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup',
-                       'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple',
-                       'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
-                       'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed',
-                       'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote',
-                       'keyboard', 'cell phone', 'microwave', 'oven', 'toaster',
-                       'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors',
-                       'teddy bear', 'hair drier', 'toothbrush']
+        class_names = ['BG', 'person', 'bicycle', 'car', 'motorcycle',
+                       'bus', 'train', 'truck'
+		      ]
 
         self.model = model
         self.config = config
